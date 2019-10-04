3fcdb1c59dfd5155d7b29d96e387445e843292cc
Connor <connorwong93@gmail.com>
2019-10-04 15:56:34 -0700

    input page content with js, just need to fix mobile view of project information

--- script.js
+++ script.js
@@ -10,8 +10,40 @@ function renderBio() {
     `);
 }
 
+function renderProjectDescription1() {
+    $('.description').html(`
+    
+    This application quizzes the user on basic information about the National Basketball Association. It is comprised of five questions of 
+    what I believe an individual should know, before calling themselves a competent NBA fan. Are you a real fan?
+    
+    <a href="https://connorhw.github.io/portfolio/" class="liveLink1">HERE!</a>
+    `);
+}
+
+function renderProjectScreenshot1() {
+    $('.screenshot').html(`
+    <img class="screenshot" src="https://user-images.githubusercontent.com/22334563/66241810-6ce48a80-e6ef-11e9-90eb-716023bd3d97.PNG">
+    `);
+} 
+
+function renderProfilePic() {
+    $('.pic').html(`
+    <img class="pic" src="https://user-images.githubusercontent.com/22334563/66242581-6525e580-e6f1-11e9-8b19-728ff3179822.png">
+    `);
+}
+
+function renderFooterLinks() {
+    $('.email').html(`connorwong93@gmail.com`);
+    $('.linkedIn').html(`<a href="https://www.linkedin.com/in/connor-wong-46b157a0/">LinkedIn</a>`);
+    $('.github').html(`<a href="https://github.com/connorhw">GitHub</a>`);
+}
+
 function renderPage() {
     renderBio();
+    renderProjectDescription1();
+    renderProjectScreenshot1();
+    renderProfilePic();
+    renderFooterLinks();
 }
 
 $(renderPage);
