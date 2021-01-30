2cdad8a9e0997d19ee0ca983ad446a8da9a1fa63
connorhw <connorwong93@gmail.com>
2021-01-29 23:01:47 -0500

    adds live vercal link for prod

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function renderBio() {
 /* -- Project 3, below -- */
 function renderProjectDescription3() {
     $('#d3').html(`
-    Currently deploying - I needed a quick and easy-to-use journal, in order to keep track of all the plant-maintenance related 
+    I needed a quick and easy-to-use journal, in order to keep track of all the plant-maintenance related 
     knowledge I'd been unconsciously accumulating over the years. The app is programmed with CRUD capabilities. You're able to submit, read, edit, 
     and delete entries that reside in their own naturalized database.
     
